7714b9b306ca42cdb551fc9978519c7f7db77f0a
Duncan Stuart <dgmstuart@gmail.com>
2014-03-12 18:27:26 +0000

    Initial commit - generated using the plugin generator

---
+++ dxw-security-plugin.php
@@ -0,0 +1,8 @@
+<?php
+// Plugin Name: dxw Security
+// Plugin URI: https://security.dxw.com/plugin
+// Description: Pulls plugin review information from dxw Security into the wordpress plugins screen
+// Version: 0.0.0
+// License: GPLv2
+// Author: dxw
+// Author URI: http://dxw.com/
