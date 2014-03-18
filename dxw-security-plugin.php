ee70fa78951035dd27e4f0d89763e36fd1f7c1c4
Duncan Stuart <dgmstuart@gmail.com>
2014-03-18 20:28:46 +0000

    Major refactoring/cleanup and some tweaks to the presentation

--- dxw-security-plugin.php
+++ dxw-security-plugin.php
@@ -21,6 +21,7 @@ define('DXW_SECURITY_FAILURE_lIMIT', 5);
 // The URL we link to when we don't have any info about a plugin
 define('DXW_SECURITY_PLUGINS_URL', 'https://security.dxw.com/plugins/');
 
+
 add_action( 'admin_enqueue_scripts', function($hook) {
   if( 'plugins.php' != $hook ) { return; }
 
@@ -32,7 +33,14 @@ add_action('admin_init', function() { new Dxw_Security_Column; });
 
 class Dxw_Security_Column {
   // Track the number of failed requests so that we can stop trying after a certain number.
-  var $dxw_security_failures = 0;
+  public $dxw_security_failures = 0;
+
+  # TODO: this should be some kind of constant, but we couldn't work out how. Static didn't work, and class consts can't contain arrays
+  public $review_statuses = array(
+    'green'  => array( 'message' => "No issues found",  'slug' => "no_issues_found", 'failure' => false, 'icon_fallback' => "&#10003;"),
+    'yellow' => array( 'message' => "Use with caution", 'slug' => "use_with_caution", 'failure' => true, 'icon_fallback' => "?"),
+    'red'    => array( 'message' => "Potentially unsafe", 'slug' => "potentially_unsafe", 'failure' => true, 'icon_fallback' => "&#10007;"),
+  );
 
   public function __construct() {
     add_filter('plugin_row_meta', function( $plugin_meta, $plugin_file, $plugin_data, $status) {
@@ -43,10 +51,7 @@ class Dxw_Security_Column {
   }
 
   function security_plugin_meta($plugin_file, $plugin_data) {
-    $plugin_version = $plugin_data['Version'];
-    $response = $this->get_plugin_review_response($plugin_file, $plugin_version);
-
-    $review_link = DXW_SECURITY_PLUGINS_URL;
+    $response = $this->get_plugin_review_response($plugin_file, $plugin_data);
 
     if ( is_wp_error($response) ) {
       # TODO: in future we should provide some way for users to give us back some useful information when they get this error
@@ -58,24 +63,11 @@ class Dxw_Security_Column {
         case 200:
           $review = json_decode( $response['body'] )->review;
 
-          $recommendation = $review->recommendation;
+          $status = $this->review_statuses[$review->recommendation];
+          $message = $status['message'];
           $review_link = $review->review_link;
 
-          switch ($recommendation) {
-            case 'green':
-              $message = "No issues found";
-              break;
-            case 'yellow':
-              $message = "Use with caution";
-              $this->add_review_reason($plugin_file);
-              break;
-            case 'red':
-              $message = "Potentially unsafe";
-              $this->add_review_reason($plugin_file);
-              break;
-            // default:
-            //   TODO: error??? This is definitely an exception case;
-          };
+          if ( $status['failure'] ) { $this->add_review_reason($plugin_file); }
           break;
         case 404:
           $message = "No info";
@@ -93,49 +85,15 @@ class Dxw_Security_Column {
           $message = "An error occurred - please try again later";
       };
 
-      return "dxw Security review: <a href='{$review_link}'>{$message}</a>";
+      if ( empty($review_link) ) { $review_link = DXW_SECURITY_PLUGINS_URL; }
+
+      return "dxw Security recommendation: <a href='{$review_link}'>{$message}</a>";
     };
   }
 
 
-  // function security_column_content($plugin_file, $plugin_version) {
-  //   if ( $this->dxw_security_failures >= DXW_SECURITY_FAILURE_lIMIT ) {
-  //     echo $this->fatal_error();
-  //     return;
-  //   }
-
-  //   $response = $this->get_plugin_review_response($plugin_file, $plugin_version);
-
-  //   if ( is_wp_error($response) ) {
-  //     # TODO: in future we should provide some way for users to give us back some useful information when they get this error
-  //     echo $this->fatal_error();
-
-  //   } else {
-
-  //     switch ( $response['response']['code'] ) {
-  //       case 200:
-  //         $review = json_decode( $response['body'] )->review;
-  //         echo $this->display_status_icon($plugin_file, $review);
-  //         break;
-  //       case 404:
-  //         echo $this->no_info();
-  //         break;
-  //       // TODO: Perhaps the following won't ever happen? Will just be caught by WP_Error?
-  //       //   Perhaps we should behave differently in different cases
-  //       case 400:
-  //         echo $this->fatal_error();
-  //         break;
-  //       case 500:
-  //         echo $this->fatal_error();
-  //         break;
-  //       default:
-  //         // A redirect would end up here - is it possible to get one??
-  //         echo $this->fatal_error();
-  //     };
-  //   };
-  // }
-
-  private function get_plugin_review_response($plugin_file, $plugin_version) {
+  private function get_plugin_review_response($plugin_file, $plugin_data) {
+    $plugin_version = $plugin_data['Version'];
     $response = $this->retrieve_plugin_review_response($plugin_file, $plugin_version);
 
     if($response === false) {
@@ -184,10 +142,7 @@ class Dxw_Security_Column {
   private function add_review_reason($plugin_file) {
     // add_action( "after_plugin_row_$plugin_file", function ($file, $plugin_data) {
     add_action( "after_plugin_row_$plugin_file", function($plugin_file, $plugin_data, $status) {
-      // TODO: repeated...
-      $plugin_version = $plugin_data['Version'];
-
-      $response = $this->get_plugin_review_response($plugin_file, $plugin_version);
+      $response = $this->get_plugin_review_response($plugin_file, $plugin_data);
 
       if ( is_wp_error($response) ) {
         // Do nothing - shouldn't get here
@@ -198,25 +153,12 @@ class Dxw_Security_Column {
           case 200:
             $review = json_decode( $response['body'] )->review;
 
-            $recommendation = $review->recommendation;
+            // TODO: Defensive programming? We're currently trusting that this will only ever get called with "red" or "yellow" recommendations
+            $status = $this->review_statuses[$review->recommendation];
+            $message = $status['message'];
+            $box_class = $status['slug'];
             $review_link = $review->review_link;
             $reason = $review->reason;
-
-            switch ($recommendation) {
-              case 'green':
-                // Do nothing - shouldn't get here
-                break;
-              case 'yellow':
-                $message = "Use with caution";
-                $box_class = "use_with_caution";
-                break;
-              case 'red':
-                $message = "Potentially unsafe";
-                $box_class = "potentially_unsafe";
-                break;
-              // default:
-              //   TODO: error??? This is definitely an exception case;
-            };
             break;
           case 404:
             // Do nothing - shouldn't get here
@@ -235,9 +177,7 @@ class Dxw_Security_Column {
         };
       };
 
-      // Defaults:
       if ( empty($review_link) ) { $review_link = DXW_SECURITY_PLUGINS_URL; }
-      if ( empty($reason) ) { $reason = "See the dxw Security website for details"; }
 
       $row_class = $this->row_class($plugin_file, $plugin_data);
 
@@ -245,8 +185,13 @@ class Dxw_Security_Column {
       echo("<tr class='plugin-review-tr {$row_class}'>");
       echo("  <td colspan='4' class='plugin-review colspanchange'>");
       echo("    <div class='review-message {$box_class}'>");
-      echo("      <a href='{$review_link}'><h4>dxw Security review: {$message}</h4></a>");
-      print_r($reason);
+      echo("      <a href='{$review_link}'><h4>dxw Security recommendation: {$message}</h4></a>");
+      if ( empty($reason) ) {
+        echo("<a href='{$review_link}'>See the dxw Security website for details</a>");
+      } else {
+        print_r($reason);
+        echo("<a href='{$review_link}'> Read more...</a>");
+      }
       echo("</div></td></tr>");
     }, 10, 3);
   }
@@ -259,49 +204,9 @@ class Dxw_Security_Column {
     if (! empty( $plugin_data['update'] ) ) { $class .= ' update_plugin'; }
     return $class;
   }
-
-  // private function display_status_icon($plugin_file, $review) {
-  //   $link = $review->review_link;
-  //   $recommendation = $review->recommendation;
-
-  //   switch ($recommendation) {
-  //     case 'green':
-  //       return $this->no_issues_found($link);
-  //     case 'yellow':
-  //       $this->add_review_failure_reason($plugin_file);
-  //       return $this->use_with_caution($link);
-  //     case 'red':
-  //       $this->add_review_failure_reason($plugin_file);
-  //       return $this->potentially_unsafe($link);
-  //     // default:
-  //     //   TODO: error??? This is definitely an exception case;
-  //   };
-  // }
-
-  private function no_issues_found($link) {
-    return $this->status_icon($link, "&#10003;", "status no_issues_found", "No issues found");
-  }
-  private function use_with_caution($link) {
-    return $this->status_icon($link, "?", "status use_with_caution", "Use with caution");
-  }
-  private function potentially_unsafe($link) {
-    return $this->status_icon($link, "&#10007;", "status potentially_unsafe", "Potentially unsafe");
-  }
-  private function no_info() {
-    return $this->status_icon(DXW_SECURITY_PLUGINS_URL, "-", "status no_info", "This plugin has not yet been reviewed by dxw");
-  }
-  private function fatal_error() {
-    $this->dxw_security_failures++;
-    return $this->status_icon(DXW_SECURITY_PLUGINS_URL, "??", "fatal_error", "Sorry - we couldn't connect to the dxw Security service. Please try again later");
-  }
-
-
-  private function status_icon($link, $character, $class, $title_text) {
-    return "<a href='" . $link . "' title='" . esc_html($title_text)
-     . "' class='" . $class . "''>" . $character . "</a>";
-  }
 }
 
+// CURRENTLY NOT USED - ditch it if it's not useful
 // Surely this already exists???
 function insert_at($index, $element, $array) {
   $array =
