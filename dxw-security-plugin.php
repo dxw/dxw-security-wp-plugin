0c58ded1212bc40d56dbca421c10ab2ce5a571e0
Duncan Stuart <dgmstuart@gmail.com>
2014-03-18 17:55:47 +0000

    Adding the review reason to the plugin row

--- dxw-security-plugin.php
+++ dxw-security-plugin.php
@@ -21,18 +21,14 @@ define('DXW_SECURITY_FAILURE_lIMIT', 5);
 // The URL we link to when we don't have any info about a plugin
 define('DXW_SECURITY_PLUGINS_URL', 'https://security.dxw.com/plugins/');
 
-
 add_action( 'admin_enqueue_scripts', function($hook) {
-  if( 'plugins.php' != $hook )
-    return;
+  if( 'plugins.php' != $hook ) { return; }
+
   $stylesheet_url = plugins_url( '/styles/style.css' , __FILE__ );
   wp_enqueue_style( 'dxw-security-plugin-styles', $stylesheet_url );
 } );
 
-add_action('admin_init', function() {
-  $column = new Dxw_Security_Column;
-});
-
+add_action('admin_init', function() { new Dxw_Security_Column; });
 
 class Dxw_Security_Column {
   // Track the number of failed requests so that we can stop trying after a certain number.
@@ -40,123 +36,179 @@ class Dxw_Security_Column {
 
   public function __construct() {
     add_filter('manage_plugins_columns', function($columns) {
-      return insertAt(1, array(DXW_SECURITY_COLUMN_NAME => ""), $columns);
+      return insert_at(1, array(DXW_SECURITY_COLUMN_NAME => ""), $columns);
     });
 
     add_action('manage_plugins_custom_column',function($column_name, $plugin_file, $plugin_data){
       if($column_name == DXW_SECURITY_COLUMN_NAME) {
-        # TODO: Currently this only handles codex plugins
-        $plugin_link = 'http://wordpress.org/plugins/' . explode('/',$plugin_file)[0] . '/';
         $plugin_version = $plugin_data['Version'];
-
-        $this->securityColumnContent($plugin_link, $plugin_version);
+        $this->security_column_content($plugin_file, $plugin_version);
       };
-    }, 10, 3);
+    }, 11, 3);
   }
 
 
-  function securityColumnContent($plugin_link, $plugin_version) {
+  function security_column_content($plugin_file, $plugin_version) {
     if ( $this->dxw_security_failures >= DXW_SECURITY_FAILURE_lIMIT ) {
-      echo $this->fatalError();
+      echo $this->fatal_error();
       return;
     }
 
-    $response = $this->getPluginReview($plugin_link, $plugin_version);
+    $response = $this->get_plugin_review_response($plugin_file, $plugin_version);
 
     if ( is_wp_error($response) ) {
       # TODO: in future we should provide some way for users to give us back some useful information when they get this error
-      echo $this->fatalError();
+      echo $this->fatal_error();
 
     } else {
 
       switch ( $response['response']['code'] ) {
         case 200:
-          $response_body = json_decode( $response['body'] )->review;
-          echo $this->displayStatusIcon($response_body->review_link, $response_body->recommendation);
+          $review = json_decode( $response['body'] )->review;
+          echo $this->display_status_icon($plugin_file, $review);
           break;
         case 404:
-          echo $this->noInfo();
+          echo $this->no_info();
           break;
         // TODO: Perhaps the following won't ever happen? Will just be caught by WP_Error?
         //   Perhaps we should behave differently in different cases
         case 400:
-          echo $this->fatalError();
+          echo $this->fatal_error();
           break;
         case 500:
-          echo $this->fatalError();
+          echo $this->fatal_error();
           break;
         default:
           // A redirect would end up here - is it possible to get one??
-          echo $this->fatalError();
+          echo $this->fatal_error();
       };
     };
   }
 
-  private function getPluginReview($plugin_url, $plugin_version) {
-    $api_root = DXW_SECURITY_API_ROOT;
-    $api_path = "/reviews";
-    $query = http_build_query(
-      array(
-        'codex_link'=>$plugin_url,
-        'version'=>$plugin_version
-      )
-    );
-    // this should exist in core, but doesn't seem to:
-    // $url = http_build_url(
-    //   array(
-    //     "host"  => $api_root,
-    //     "path"  => $api_path,
-    //     "query" => $query
-    //   )
-    // );
-    $url = $api_root . $api_path . '?' . $query;
+  private function get_plugin_review_response($plugin_file, $plugin_version) {
+    $response = $this->retrieve_plugin_review_response($plugin_file, $plugin_version);
 
-    $result = wp_remote_get($url);
-    return $result;
+    if($response === false) {
+
+      $api_root = DXW_SECURITY_API_ROOT;
+      $api_path = "/reviews";
+
+      # TODO: Currently this only handles codex plugins
+      $plugin_url = 'http://wordpress.org/plugins/' . explode('/',$plugin_file)[0] . '/';
+
+      $query = http_build_query(
+        array(
+          'codex_link'=>$plugin_url,
+          'version'=>$plugin_version
+        )
+      );
+      // this should exist in core, but doesn't seem to:
+      // $url = http_build_url(
+      //   array(
+      //     "host"  => $api_root,
+      //     "path"  => $api_path,
+      //     "query" => $query
+      //   )
+      // );
+      $url = $api_root . $api_path . '?' . $query;
+
+      $response = wp_remote_get($url);
+      $this->cache_plugin_review_response($plugin_file, $plugin_version, $response);
+    }
+
+    return $response;
   }
 
-  private function displayStatusIcon($link, $recommendation) {
+  private function cache_plugin_review_response($plugin_file, $plugin_version, $response) {
+    $slug = $this->plugin_review_response_slug($plugin_file, $plugin_version);
+    set_transient( $slug, $response, DAY_IN_SECONDS );
+  }
+  private function retrieve_plugin_review_response($plugin_file, $plugin_version) {
+    $slug = $this->plugin_review_response_slug($plugin_file, $plugin_version);
+    return get_transient($slug);
+  }
+  private function plugin_review_response_slug($plugin_file, $plugin_version) {
+    return $plugin_file . $plugin_version;
+  }
+
+  private function add_review_failure_reason($plugin_file) {
+    // add_action( "after_plugin_row_$plugin_file", function ($file, $plugin_data) {
+    add_action( "after_plugin_row_$plugin_file", function($plugin_file, $plugin_data, $status) {
+      // TODO: repeated...
+      $plugin_version = $plugin_data['Version'];
+
+      $review_response = $this->get_plugin_review_response($plugin_file, $plugin_version);
+
+      // TODO: repeated...
+      $review = json_decode( $review_response['body'] )->review;
+
+      $reason = $review->reason;
+
+      // TODO: Make both of the following into links
+      if (!$reason) { $reason = "See the dxw Security website for details"; };
+
+      $class = $this->row_class($plugin_file, $plugin_data);
+
+      // Presumably colspanchange is something to do with responsiveness
+      echo("<tr class='plugin-review-tr {$class}'><td colspan='4' class='plugin-review colspanchange'><div class='review-message'>");
+      print_r($reason);
+      echo("</div></td></tr>");
+    }, 10, 3);
+  }
+
+  private function row_class($plugin_file, $plugin_data) {
+    // mostly cribbed from the WP_Plugins_List_Table:
+    // TODO: This doesn't handle multisite (?)
+    $class = "";
+    if ( is_plugin_active( $plugin_file ) ) { $class .= 'active_plugin'; }
+    if (! empty( $plugin_data['update'] ) ) { $class .= ' update_plugin'; }
+    return $class;
+  }
+
+  private function display_status_icon($plugin_file, $review) {
+    $link = $review->review_link;
+    $recommendation = $review->recommendation;
+
     switch ($recommendation) {
       case 'green':
-        return $this->noIssuesFound($link);
-        break;
+        return $this->no_issues_found($link);
       case 'yellow':
-        return $this->useWithCaution($link);
-        break;
+        $this->add_review_failure_reason($plugin_file);
+        return $this->use_with_caution($link);
       case 'red':
-        return $this->potentiallyUnsafe($link);
-        break;
+        $this->add_review_failure_reason($plugin_file);
+        return $this->potentially_unsafe($link);
       // default:
       //   TODO: error??? This is definitely an exception case;
     };
   }
 
-  private function noIssuesFound($link) {
-    return $this->statusIcon($link, "&#10003;", "status no_issues_found", "No issues found");
+  private function no_issues_found($link) {
+    return $this->status_icon($link, "&#10003;", "status no_issues_found", "No issues found");
   }
-  private function useWithCaution($link) {
-    return $this->statusIcon($link, "?", "status use_with_caution", "Use with caution");
+  private function use_with_caution($link) {
+    return $this->status_icon($link, "?", "status use_with_caution", "Use with caution");
   }
-  private function potentiallyUnsafe($link) {
-    return $this->statusIcon($link, "&#10007;", "status potentially_unsafe", "Potentially unsafe");
+  private function potentially_unsafe($link) {
+    return $this->status_icon($link, "&#10007;", "status potentially_unsafe", "Potentially unsafe");
   }
-  private function noInfo() {
-    return $this->statusIcon(DXW_SECURITY_PLUGINS_URL, "-", "status no_info", "This plugin has not yet been reviewed by dxw");
+  private function no_info() {
+    return $this->status_icon(DXW_SECURITY_PLUGINS_URL, "-", "status no_info", "This plugin has not yet been reviewed by dxw");
   }
-  private function fatalError() {
+  private function fatal_error() {
     $this->dxw_security_failures++;
-    return $this->statusIcon(DXW_SECURITY_PLUGINS_URL, "??", "fatal_error", "Sorry - we couldn't connect to the dxw Security service. Please try again later");
+    return $this->status_icon(DXW_SECURITY_PLUGINS_URL, "??", "fatal_error", "Sorry - we couldn't connect to the dxw Security service. Please try again later");
   }
 
 
-  private function statusIcon($link, $character, $class, $title_text) {
+  private function status_icon($link, $character, $class, $title_text) {
     return "<a href='" . $link . "' title='" . esc_html($title_text)
      . "' class='" . $class . "''>" . $character . "</a>";
   }
 }
 
 // Surely this already exists???
-function insertAt($index, $element, $array) {
+function insert_at($index, $element, $array) {
   $array =
     array_slice($array, 0, $index, true) +
     $element +
