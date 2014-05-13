0e6c8454e8ebd55642d3b3438b4f14988a4af799
Duncan Stuart <dgmstuart@gmail.com>
2014-05-13 16:28:41 +0100

    The installed version number is now displayed on the dialog link

--- dxw-security.php
+++ dxw-security.php
@@ -128,11 +128,12 @@ class Plugin_Recommendation {
   private $review_data;
   private $css_class;
 
-  public function __construct($name, $version, $slug, $review_data, $more_info="More information", $dialog_intro="", $css_class="") {
+  public function __construct($name, $version, $slug, $review_data, $heading, $more_info="More information", $dialog_intro="", $css_class="") {
     $this->name = $name;
     $this->version = $version;
     $this->slug = $slug;
     $this->review_data = $review_data;
+    $this->heading = $heading;
     $this->more_info = $more_info;
     $this->dialog_intro = $dialog_intro;
     $this->css_class = $css_class;
@@ -141,7 +142,7 @@ class Plugin_Recommendation {
   public function render() {
     ?>
       <a href="#<?php echo esc_attr($this->dialog_id()); ?>" data-title="<?php echo esc_attr($this->name); ?> - <?php echo esc_attr($this->version); ?>" class="dialog-link review-message <?php echo esc_attr($this->slug) ?> <?php echo esc_attr($this->css_class) ?>">
-        <h3><?php echo $this->review_data->heading(); ?></h3>
+        <h3><?php echo $this->heading; ?></h3>
 
         <p class="more-info"><?php echo esc_html($this->more_info); ?></p>
       </a>
@@ -172,7 +173,7 @@ class Plugin_Recommendation {
 
 class Plugin_Recommendation_Reviewed {
   public function __construct($name, $version, $review_data) {
-    $this->recommendation = new Plugin_Recommendation($name, $version, $review_data->slug, $review_data);
+    $this->recommendation = new Plugin_Recommendation($name, $version, $review_data->slug, $review_data, $review_data->heading());
   }
   public function render() {
     $this->recommendation->render();
@@ -186,8 +187,9 @@ class Plugin_Recommendation_Other_Versions_Reviewed {
   public function __construct($name, $version, $other_reviews_data) {
     $latest_result = $other_reviews_data->most_recent()->slug;
     $dialog_intro =  "<p class='intro'>The installed version ({$version}) has not yet been reviewed, but here are some reviews of other versions:</p>";
+    $heading = "This version ({$version}) not yet reviewed";
     $more_info =  "These versions have been reviewed: {$other_reviews_data->versions()}";
-    $this->recommendation = new Plugin_Recommendation($name, $version, "other-versions-reviewed", $other_reviews_data, $more_info, $dialog_intro, "other-{$latest_result}");
+    $this->recommendation = new Plugin_Recommendation($name, $version, "other-versions-reviewed", $other_reviews_data, $heading, $more_info, $dialog_intro, "other-{$latest_result}");
   }
   public function render() {
     $this->recommendation->render();
@@ -285,10 +287,6 @@ class Other_Version_Reviews_Data {
         </div>
       <?php
     }
-  }
-
-  public function heading() {
-    return "This version not yet reviewed";
   }
 
   public function most_recent() {
