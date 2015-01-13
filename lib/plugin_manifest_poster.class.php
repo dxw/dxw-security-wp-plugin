ed3a70e96180a788504b235602d7da0423551d18
Duncan Stuart <dgmstuart@gmail.com>
2015-01-13 15:02:00 +0000

    Defensive programming? (untested) - handling the case where the error message on a failed manifest post returns an error with an empty error message

--- lib/plugin_manifest_poster.class.php
+++ lib/plugin_manifest_poster.class.php
@@ -23,15 +23,22 @@ class dxw_security_Plugin_Manifest_Poster {
 
   private static function handle_api_error($e) {
     $subject = "Your WordPress site failed to send a plugin list to dxw Security";
+    $error_message = self::error_message($e);
     $message = <<<EOF
 Your WordPress site is configured to regularly send a list of your plugins to dxw Security.
 The most recent time it tried to do this, it failed with the following message:
 
-{$e->getMessage()}
+{$error_message}
 
 You might want to consider forwarding this email to security@dxw.com to see if we can diagnose the issue.
 EOF;
     dxw_security_Email::deliver($subject, $message);
+  }
+
+  private static function error_message($error) {
+    $message = $error->getMessage();
+    if ( empty($message) ) { $message = "(no message provided)"; }
+    return $message;
   }
 }
 
