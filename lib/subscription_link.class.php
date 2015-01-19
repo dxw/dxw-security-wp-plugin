2fd4616360d41c0e6d39a51224c2ae7cbc12b5fd
Duncan Stuart <dgmstuart@gmail.com>
2015-01-19 15:55:22 +0000

    The button on the activation banner links to the settings page

--- lib/subscription_link.class.php
+++ lib/subscription_link.class.php
@@ -2,6 +2,8 @@
 
 defined('ABSPATH') OR exit;
 
+require_once(dirname(__FILE__) . '/settings_page.class.php');
+
 class dxw_security_Subscription_Link {
 
   public static function can_subscribe() {
@@ -9,8 +11,9 @@ class dxw_security_Subscription_Link {
   }
 
   public static function render() {
+    $url = dxw_security_Settings_Page::url();
     ?>
-      <a href="#" class="alert_subscription_button button-primary">Subscribe to alerts</a>
+      <a href="<?php echo $url ?>" class="alert_subscription_button button-primary">Subscribe to alerts</a>
     <?php
   }
 }
